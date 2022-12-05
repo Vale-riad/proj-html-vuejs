b90abf23df376749d977abbe1e6738044d86f4ce
Vale-riad <valentinariad91@gmail.com>
2022-12-05 13:45:12 +0100

    Aggiunge hero e title

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,6 +11,10 @@ export default {
         <img src="../assets/avada-music-logo.png" />
         <i class="fa-solid fa-bars"></i>
       </div>
+      <div class="header-bottom">
+        <h1 class="title">Untold Stories</h1>
+        <p><em>There is an untold story behind every favourite song.</em></p>
+      </div>
     </div>
   </header>
 </template>
@@ -21,7 +25,7 @@ header {
   background-image: url(../assets/home_slider.jpg);
   background-size: cover;
   background-repeat: no-repeat;
-  height: 1000px;
+  height: 900px;
   img {
     height: 50px;
   }
@@ -31,6 +35,26 @@ header {
     align-items: center;
   }
   i {
+    color: #ffffff;
+  }
+  .header-bottom {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 300px;
+  }
+  .title {
+    flex-direction: column;
+    align-items: center;
+  }
+  .title {
+    font-size: 100px;
+  }
+  .title {
+    color: #ffffff;
+    margin-right: -20px;
+  }
+  p {
     color: #ffffff;
   }
 }
